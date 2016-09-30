b0567b96a9db73dfee06eb7f472eec430dd29e4e
Khaled_Craft1b <iiBa4x@gmail.com>
2016-09-30 22:26:35 +0300

    Update Main.php

--- src/WorldChat/Main.php
+++ src/WorldChat/Main.php
@@ -2,11 +2,11 @@
 namespace WorldChat;
 class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
   public function onEnable(){
-    $this->getServer()->getPluginManager ()->registerEvents ( $this , $this );
+    $this->getServer()->getPluginManager()->registerEvents($this , $this);
   }
   public function onChat(\pocketmine\event\player\PlayerChatEvent $event){
     $event->setCancelled();
-    $event->getPlayer()->level->getPlayers()->sendMessage("[".$event->getPlayer()->getNameTag()."] > ".$event->getMessage());
+    $event->getPlayer()->level->getPlayers()->sendMessage($event->getPlayer()->getNameTag()." > ".$event->getMessage());
   }
 }
 ?>
