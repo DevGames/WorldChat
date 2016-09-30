d3d6409f7c578ed5b8ef1104df2b5449a38539d6
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-09-30 12:13:11 -0700

    Update Main.php

--- src/WorldChat/Main.php
+++ src/WorldChat/Main.php
@@ -6,7 +6,7 @@ class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Li
   }
   public function onChat(\pocketmine\event\player\PlayerChatEvent $event){
     $event->setCancelled();
-    $event->getPlayer()->level->getPlayers()->sendMessage($event->getMessage());
+    $event->getPlayer()->level->getPlayers()->sendMessage("[".$event->getPlayer()->getNameTag()."] > ".$event->getMessage());
   }
 }
 ?>
